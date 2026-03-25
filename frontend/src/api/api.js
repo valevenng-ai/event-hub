0efc024e2e2c1d9986ea84a30bded3a9339fea64
valevenng-ai <val.even.ng@gmail.com>
2026-03-25 22:44:53 +0100

    architecture et pages

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,3 +3,14 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE
 })
+
+// Pour ajouter automatiquement le token dans les requête
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
